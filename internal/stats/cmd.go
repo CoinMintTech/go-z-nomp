9fc8125771db586f7f462c2261a546883f5a064b
Pedro Salgado <steenzout@users.noreply.github.com>
2018-08-24 22:02:13 -0600

    internal: added package for command-line interface subcommands.

---
+++ internal/stats/cmd.go
@@ -0,0 +1,33 @@
+//
+// Copyright 2018 Pedro Salgado
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package stats
+
+import (
+	"fmt"
+
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+// ValidateArg nothing to be validated.
+func ValidateArg(c *cli.Context) error {
+	return nil
+}
+
+// Run a.
+func Run(cctx *cli.Context) error {
+	return fmt.Errorf("not implemented")
+}
